00dc18547fe2f3167ec97c342d6ce2fa393b4b05
mrtensai <zaki.oussama@gmail.com>
2013-12-03 01:08:44 +0000

    Create puissance.pas
    calculer la puissance mathématique x^n

---
+++ puissance.pas
@@ -0,0 +1,19 @@
+Program Puissance;
+
+var
+   x, p :real;
+   n, i :integer;
+Begin
+     Writeln(' -- Entrez x et n pour calculer x^n -- ');
+     Write(' x = ');
+     readln(x);
+     Write(' n = ');
+     readln(n);
+
+     p := 1;
+     For i := 1 To n do
+         p := p * x;
+
+     writeln(' x^n = ', p:0:0);
+     readln();
+End.
