3cf841db8c857b4e6462cd3db505946532a266bf
mrtensai <zaki.oussama@gmail.com>
2013-12-16 21:58:53 +0000

    Create Sapin.pas

---
+++ Sapin.pas
@@ -0,0 +1,46 @@
+program Sapin;
+uses Crt;
+{une procedure pour dessiner les branches}
+procedure drawFloor (N,floor,offset : integer);
+var i,j,k : integer;
+begin
+     TextColor(Green);
+     For i:= 1 To N do
+     begin
+         For j:= 1 To offset+N-i do
+             write(' ');
+         For k:= 1 To i+floor do
+             write(' *');
+         writeln;
+     end;
+end;
+{une procedure pour dessiner le tronc}
+procedure drawTronc (N,offset : integer);
+var i,j,k : integer;
+begin
+     TextColor(yellow);
+     For i:= 1 To N do
+     begin
+         For j:= 1 To offset+N do
+             write(' ');
+         For k:= 1 To N do
+             write('*');
+         writeln;
+     end;
+end;
+
+var
+   n,i : integer;
+begin
+     {Lire la taille du Sapin}
+     readln(n);
+     {dessiner les branches}
+     For i := 0 To n do
+         drawFloor(n,i,n-i);
+     {dessiner le tronc}
+     if n <= 3 then
+         drawTronc(n,2)
+     else
+         drawTronc(n,3);
+     readln();
+end.
